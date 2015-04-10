ebc75f55822ded37e2b8034e893cc20d53e6a769
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2015-04-10 10:26:53 +0200

    Bild wird nicht mehr gespiegelt

--- Testspiel/Assets/Scripts/NewHighlight.cs
+++ Testspiel/Assets/Scripts/NewHighlight.cs
@@ -6,28 +6,28 @@ public class NewHighlight : MonoBehaviour
 
     public Texture aTexture;
     private Vector3 screenPos, viewportPos;
-    private bool isSeen = false;
+    private bool isSeen;
     private GameObject ball;
 
 
     // Use this for initialization
     void Start()
     {
-        Debug.Log("Start");
+        
         ball = GameObject.FindGameObjectWithTag("Ball");
-        Debug.Log(ball.transform.position);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(ball.transform.position);
+        screenPos = Camera.main.WorldToScreenPoint(ball.transform.position);
         // Debug.Log("screenPos" + screenPos);
 
         viewportPos = Camera.main.WorldToViewportPoint(ball.transform.position);
-        //print("target is " + screenPos.x + " pixels from the left");
-        if ((0 < viewportPos.x && viewportPos.x < 1) && (0 < viewportPos.y && viewportPos.y < 1))
+        
+        if ((0 < viewportPos.x && viewportPos.x < 1) && (0 < viewportPos.y && viewportPos.y < 1)&& (viewportPos.z >0))
         {
             isSeen = true;
             //Debug.Log("is seen");
