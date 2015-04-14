27417a0f53361669666b0d389e8fe0990d8bc3d8
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2015-04-14 16:40:23 +0200

    objekt erkennt, wenn es angeschaut wird

--- Testspiel/Assets/Scripts/EyeTrTest.cs
+++ Testspiel/Assets/Scripts/EyeTrTest.cs
@@ -2,20 +2,68 @@
 using System.Collections;
 using iView;
 
-public class EyeTrTest : MonoBehaviour {
+public class EyeTrTest : GazeMonobehaviour
+{
+    Renderer rend;
 
-	// Use this for initialization
-	void Start () {
-     
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Vector3 averageGazePosition;
+    Vector2 gazePos;
+    GameObject objectInFocus;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         SampleData sample = SMIGazeController.Instance.GetSample();
-        Vector3 averageGazePosition = sample.averagedEye.gazePosInUnityScreenCoords();
-        Debug.Log("averageGazePos" + averageGazePosition);
-        
-	}
+        averageGazePosition = sample.averagedEye.gazePosInUnityScreenCoords();
+        gazePos = sample.averagedEye.gazePosInScreenCoords();
+        //Debug.Log("averageGazePos" + averageGazePosition);
+         objectInFocus = SMIGazeController.Instance.GetObjectInFocus(FocusFilter.WorldSpaceObjects);
+         if (objectInFocus.tag == "Clue")
+         {
+             rend.material.SetColor("_Color", Color.red);
+         }
+         else
+             rend.material.SetColor("_Color", Color.blue);
+
+    }
+
+
+
+    //public override void OnGazeStay(RaycastHit hit)
+    //{
+    //    if (hit.collider.gameObject.tag == "Clue")
+    //    {
+    //       // Debug.Log("Kugel");
+    //        rend.material.SetColor("_Color", Color.red);
+    //        //base.OnGazeEnter(hit);
+    //    }
+    //    else
+    //        rend.material.SetColor("_Color", Color.blue);
+    //   // Debug.Log("nix");
+    //}
+
+    //public override void OnGazeExit()
+    //{
+    //    rend.material.SetColor("_Color", Color.blue);
+    //}
+
+
+    private void OnGUI()
+    {
+
+
+        Texture2D square = new Texture2D(20, 20);
+        square.SetPixel(1, 1, Color.white);
+        square.Apply();
+        GUI.DrawTexture(new Rect(gazePos.x, gazePos.y, square.width, square.height), square);
+
+    }
+
 }
