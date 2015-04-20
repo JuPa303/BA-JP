30f9dd02e3e782aa0710aa58ce195b44c83b526d
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2015-04-20 17:20:05 +0200

    Richtung erweitert 2

--- Testspiel/Assets/Scripts/EyeTrTest.cs
+++ Testspiel/Assets/Scripts/EyeTrTest.cs
@@ -4,11 +4,12 @@ using iView;
 
 public class EyeTrTest : GazeMonobehaviour
 {
-    
+    public bool isMouseModusActive = false; 
+
+
     public bool gazeOnClue = false;
-    Vector3 averageGazePosition, gazePoint2, vectorToClue, vectorToGaze;
-    Vector3 gazePoint1;
-    Vector2 gazePos ;
+    Vector3 averageGazePosition, vectorToClue2D, vectorToGaze;
+    Vector2 gazePos, gazePoint1, gazePoint2;
     GameObject objectInFocus, clue, player;
     SampleData sample;
     private bool hasFirstPoint = false;
@@ -17,8 +18,10 @@ public class EyeTrTest : GazeMonobehaviour
     // Use this for initialization
     void Start()
     {
-        
-       // player = GameObject.FindGameObjectWithTag("Player");
+
+
+
+        // player = GameObject.FindGameObjectWithTag("Player");
         //clue = player.GetComponent<FindClosestClue>().FindClue();
 
     }
@@ -26,38 +29,67 @@ public class EyeTrTest : GazeMonobehaviour
     // Update is called once per frame
     void Update()
     {
-        clue = GetComponent<FindClosestClue>().FindClue(); 
+        clue = GetComponent<FindClosestClue>().FindClue();
         //Debug.Log("get clue " + clue);
         sample = SMIGazeController.Instance.GetSample();
         averageGazePosition = sample.averagedEye.gazePosInUnityScreenCoords();
         gazePos = sample.averagedEye.gazePosInScreenCoords();
-        
-        Debug.Log("averageGazePos" + averageGazePosition);
-        
+
+        //Debug.Log("averageGazePos" + averageGazePosition);
+
         getGazes();
-        //checkGazeOnObject();
-       
+        checkGazeOnObject();
+
     }
 
 
 
     private void getGazes()
     {
-
-        if (hasFirstPoint == false)
+        //Debugmode is Active
+        if (!isMouseModusActive)
         {
-            
-            gazePoint1 = sample.averagedEye.eyePosition;
-            Debug.Log("gazePoint1 "+ gazePoint1);
+            if (hasFirstPoint == false || gazePoint1 == Vector2.zero)
+            {
 
+                gazePoint1 = sample.averagedEye.eyePosition;
+                //Debug.Log("gazePoint1 "+ gazePoint1);
+                //calcDirection();
+
+                hasFirstPoint = true;
+
+            }
+            else
+            {
+
+                gazePoint2 = sample.averagedEye.eyePosition;
+                //Debug.Log("gazePoint2 " + gazePoint2);
+                calcDirection();
+            }
         }
+
+            //DebugMode
         else
         {
-           
-            gazePoint2 = sample.averagedEye.eyePosition;
-            Debug.Log("gazePoint2 " + gazePoint2);
-            calcDirection();
+            if (hasFirstPoint == false || gazePoint1 == Vector2.zero)
+            {
+
+                gazePoint1 = Input.mousePosition;
+                //Debug.Log("gazePoint1 "+ gazePoint1);
+                //calcDirection();
+
+                hasFirstPoint = true;
+
+            }
+            else
+            {
+
+                gazePoint2 = Input.mousePosition;
+                //Debug.Log("gazePoint2 " + gazePoint2);
+                calcDirection();
+            }
         }
+
 
     }
 
@@ -66,28 +98,79 @@ public class EyeTrTest : GazeMonobehaviour
     private void calcDirection()
     {
 
-        //Vektoren sollen 2D sein, da sonst die Berechnung nicht stimmt, ÄNDERN
-        Vector3[] vectors;
-        vectors = new Vector3[2];
+       
+        Vector2[] vectors;
+        vectors = new Vector2[2];
 
         vectors[0] = gazePoint1;
         vectors[1] = gazePoint2;
 
-        vectorToClue = clue.transform.position - gazePoint1;
-        gazePoint2 = sample.averagedEye.eyePosition;
+
+        Vector3 cluePos = clue.transform.position;
+        vectorToClue2D = new Vector2(cluePos.x, cluePos.y) - gazePoint1;
+
+        //Debug.Log("clue position " + cluePos);
+        //Debug.Log("gazepoint 1 " + gazePoint1);
+        //Debug.Log("vector to clue" + vectorToClue2D);
+
+       // Debug.Log("gazepoint 2 " + gazePoint2);
+
+
+        //gazePoint2 = sample.averagedEye.eyePosition;
         vectorToGaze = gazePoint2 - gazePoint1;
-        angle = Vector3.Angle(vectorToClue, vectorToGaze);
+       // Debug.Log("vector to gaze " + vectorToGaze);
 
-        if (angle <= 10)
+
+        float distanceOfGazeVectors = Vector3.Distance(gazePoint1, gazePoint2);
+
+       // Debug.Log("Distance:" + distanceOfGazeVectors);
+
+        if(distanceOfGazeVectors<=1)
         {
-            //don't show clue image
-           Debug.Log("in richtige Richtung");
+            hasFirstPoint = true;
+            gazeOnClue = false;
+           // Debug.Log("skip");
         }
+        else
+        {
+           // Debug.Log("next");
+            angle = Vector3.Angle(vectorToClue2D, vectorToGaze);
 
-        hasFirstPoint = false;
-        
+            if (angle <= 10)
+            {
+                //don't show clue image
+
+                gazeOnClue = true;
+                Debug.Log("in richtige Richtung");
+            }
+
+            hasFirstPoint = false;
+           
+        }
+        ////Difference between first and second gaze point should be more than 1, so it should be between 1 and -1
+        //if (((vectorToGaze.x <= 1.0f && -1.0f <= vectorToGaze.x))||(( vectorToGaze.y <= 1.0f && -1.0f <= vectorToGaze.y)))
+        //{
+
+        //    hasFirstPoint = true;
+        //    Debug.Log("skip");
+        //}
+
+        //else
+        //{
+        //    Debug.Log("next");
+        //    angle = Vector3.Angle(vectorToClue2D, vectorToGaze);
+
+        //    if (angle <= 10)
+        //    {
+        //        //don't show clue image
+        //        Debug.Log("in richtige Richtung");
+        //    }
+
+        //    hasFirstPoint = false;
+        //}
 
     }
+
 
 
     private void checkGazeOnObject()
@@ -114,8 +197,6 @@ public class EyeTrTest : GazeMonobehaviour
 
 
 
-
-
     private void OnGUI()
     {
 
@@ -129,14 +210,3 @@ public class EyeTrTest : GazeMonobehaviour
 
 }
 
-
-//foreach (Vector3 v in vectors)
-//{
-
-//    float curDistance = CalculatePathMesh(go.transform.position);
-//    if (curDistance < distance)
-//    {
-//        closest = go;
-//        distance = curDistance;
-//    }
-//}
