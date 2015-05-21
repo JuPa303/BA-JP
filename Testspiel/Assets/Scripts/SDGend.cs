c60676047cafe2e68bd38428d73b5406195294a4
JuPa303 <juliane.pawlow@stud.uni-regensburg.de>
2015-05-21 16:42:13 +0200

    End Screens eingebaut

---
+++ Testspiel/Assets/Scripts/SDGend.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SDGend : MonoBehaviour
+{
+
+    private int system =3;
+    private GameObject end;
+
+    // Use this for initialization
+    void Start()
+    {
+        end = GameObject.FindGameObjectWithTag("EndSDG");
+        end.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //get the number from toggle buttons
+    public void setSystem(int sysNumber)
+    {
+
+        system = sysNumber;
+
+
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        end.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+
+    }
+
+    public void pressOkay()
+    {
+        if (system != 3)
+        {
+            if (system == 0)
+            {
+                Application.LoadLevel("Compass");
+            }
+
+            if (system == 1)
+            {
+                Application.LoadLevel("MiniMap");
+            }
+
+            if (system == 2)
+            {
+                Application.Quit();
+            }
+        }
+
+    }
+}
